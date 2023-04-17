b216d5beaf0a5cd2a114eac287c91a6b8e4af5da
AdetusEpamus <serheigrakov@gmail.com>
2023-04-17 12:44:28 +0300

    Задание 1, сделано

---
+++ src/One.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class One {
+    /**
+     * @author Siarhei_Hrakau
+     * Date of receipt of the task: 17.04.2023
+     * Due dato of the task: 17.04.2023
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество чисел: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        // Ввод чисел
+        for (int i = 0; i < n; i++) {
+            System.out.print("Введите число #" + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+        scanner.close();
+        int shortestNumber = numbers[0];
+        int longestNumber = numbers[0];
+        // Поиск самого короткого и самого длинного числа
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] < shortestNumber) {
+                shortestNumber = numbers[i];
+            }
+            if (numbers[i] > longestNumber) {
+                longestNumber = numbers[i];
+            }
+        }
+        // Вывод результатов
+        System.out.println("Самое короткое число: " + shortestNumber + ", его длина: " + String.valueOf(shortestNumber).length());
+        System.out.println("Самое длинное число: " + longestNumber + ", его длина: " + String.valueOf(longestNumber).length());
+    }
+}
+
